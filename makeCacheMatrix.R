055186da0c15e0d6a6593cca42e1078cee961596
jzh1992 <jzh1992@qq.com>
2015-05-25 02:28:44 +0800

    git changes

---
+++ makeCacheMatrix.R
@@ -0,0 +1,15 @@
+## This function creates a special "Matrix", which is really
+## a list containing get and set functions.
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+}
